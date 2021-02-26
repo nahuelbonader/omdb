2384ad4e6a6c2b028d138898a914ca875f584508
Nahuel <bonadernahuel@hotmail.com>
2021-02-26 19:18:17 -0300

    Header responsive faltan detalles

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { BsFillXCircleFill } from "react-icons/bs";
+import { BsX } from "react-icons/bs";
 import style from "./style.module.scss";
 
 export default ({ placeholder, handleChange, value, cleanValue }) => (
   <div className={style.container}>
     <input placeholder={placeholder} onChange={handleChange} value={value} />
-    <BsFillXCircleFill onClick={cleanValue} className={style.cross} />
+    <BsX onClick={cleanValue} className={style.cross} />
   </div>
 );
